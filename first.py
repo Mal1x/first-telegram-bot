6eab5d5557982a61ef05a98e73197c4c3ef7bbe2
Mal1x <malixmotmax123@gmail.com>
2026-08-24 18:47:05 +0200

    Add Reply and Inline keyboards

--- first.py
+++ first.py
@@ -4,6 +4,10 @@ from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram import F
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import CallbackQuery
 
 load_dotenv()
 
@@ -12,9 +16,26 @@ TOKEN = os.getenv("BOT_TOKEN")
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+main_kb = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="Hello"), KeyboardButton(text="About")]],resize_keyboard=True)
+inline_kb = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="More info", callback_data="More_info")]])
+
+@dp.callback_query(F.data == "More_info")
+async def more_info(callback: CallbackQuery):
+    await callback.answer()
+
+    await callback.message.answer("Example of Inline button")
+
 @dp.message(Command('start'))
 async def start(message: Message):
+    await message.answer("Hello!", reply_markup=main_kb)
+
+@dp.message(F.text == "Hello")
+async def hello(message: Message):
     await message.answer("Hello")
+
+@dp.message(F.text == "About")
+async def about(message: Message):
+    await message.answer("This is a test bot.", reply_markup=inline_kb)
 
 @dp.message()
 async def text(message: Message):
@@ -22,5 +43,6 @@ async def text(message: Message):
 
 async def main():
     await dp.start_polling(bot)
+
 
 if __name__ == '__main__':  asyncio.run(main())
